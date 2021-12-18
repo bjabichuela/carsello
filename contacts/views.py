30225fd62f70d2eb0af4da779a8d5f139372f460
bjabichuela <bjabichuela@gmail.com>
2021-12-18 20:40:06 +0800

    Updated settings.py

--- contacts/views.py
+++ contacts/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 
 # Create your views here.
-def inquiry(request):
+def contact(request):
     if request.method == 'POST':
         car_id = request.POST['car_id']
         car_title = request.POST['car_title']
@@ -27,9 +27,9 @@ def inquiry(request):
                 messages.error(request, 'You have already made an inquiry about this car. Please wait until we get back to you.')
                 return redirect('/cars/'+car_id)
 
-        contact = Contact(car_id=car_id, car_title=car_title, user_id=user_id,
+        contact_info = Contact(car_id=car_id, car_title=car_title, user_id=user_id,
         first_name=first_name, last_name=last_name, customer_need=customer_need, city=city, email=email, phone=phone, message=message)
 
-        contact.save()
+        contact_info.save()
         messages.success(request, 'Your request has been submitted, we will get back to you shortly.')
         return redirect('/cars/'+car_id)
